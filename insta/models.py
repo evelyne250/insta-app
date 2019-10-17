52181441fdf3f252eac147a594c85c00ebb641ca
pascaline <passyish5@gmail.com>
2019-10-17 17:44:34 +0200

    added an admin

--- insta/models.py
+++ insta/models.py
@@ -4,5 +4,8 @@ class Image(models.Model):
     # image = models.ImageField(upload_to='pics/')
     image_name = models.CharField(max_length=30)
     description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
     # location= models.ForeignKey(Location)
     # category = models.ForeignKey(Category, db_column='names')
+    def __str__(self):
+        return self.description

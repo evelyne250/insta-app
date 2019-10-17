5eb3abdead6594ac0635506eb9b1b01dfa2593a7
pascaline <passyish5@gmail.com>
2019-10-17 18:04:06 +0200

    uploaded images

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 import datetime as dt
-
+from .models import Image
 def welcome(request):
     date = dt.date.today()
-    return render(request, 'welcome.html', {"date": date,})
+    images = Image.objects.all()
+    return render(request, 'welcome.html', {"date": date,"images":images})

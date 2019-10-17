52a554e79e6becccab243542d1829c38cbd8afa7
pascaline <passyish5@gmail.com>
2019-10-17 15:20:40 +0200

    created first model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    # image = models.ImageField(upload_to='pics/')
+    image_name = models.CharField(max_length=30)
+    description = models.TextField()
+    # location= models.ForeignKey(Location)
+    # category = models.ForeignKey(Category, db_column='names')

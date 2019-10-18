572cd20c6f945bda7c92b3bb8c4ec91b2e13335e
pascaline <passyish5@gmail.com>
2019-10-18 08:23:59 +0200

    added the login

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 import datetime as dt
 from .models import Image
+
 def welcome(request):
     date = dt.date.today()
     images = Image.objects.all()
     return render(request, 'welcome.html', {"date": date,"images":images})
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    images = Image.objects.all()
+    return render(request, 'home.html', {"images":images})

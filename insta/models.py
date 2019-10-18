7eddb8ad7b5dcd926c6c1350780e74eec86a68c4
pascaline <passyish5@gmail.com>
2019-10-18 08:59:17 +0200

    added the foreign key of User

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Image(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='pics/')
     image_name = models.CharField(max_length=30)
     description = models.TextField()
